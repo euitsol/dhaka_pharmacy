92fa20c368180c59e330f0c39287cba7e488ed57
mrsharifull <116838065+mrsharifull@users.noreply.github.com>
2023-12-20 19:16:33 +0600

    Overwrite all default route...add route prefix user/

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,32 @@ Route::get('/', function () {
 Auth::routes();
 
 
+// Overwrite Default Routes
+
+Route::prefix('user')->group(function () {
+    Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
+
+	Route::get('/register', [App\Http\Controllers\Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/register', [App\Http\Controllers\Auth\RegisterController::class, 'register']);
+
+	Route::get('/password/reset', [App\Http\Controllers\Auth\ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+    Route::post('/password/email', [App\Http\Controllers\Auth\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+    Route::get('/password/reset/{token}', [App\Http\Controllers\Auth\ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+    Route::post('/password/reset', [App\Http\Controllers\Auth\ResetPasswordController::class, 'reset']);
+});
+
+
+
+
+
+
+
+
+
+
+
+
 Route::group(['middleware' => ['auth', 'permission'],'prefix'=>'admin'], function () {
     Route::get('/dashboard', function () {
         return view('backend.dashboard.dashboard');
