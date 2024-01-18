6176035f3ba51bcabccb8bf761558978812638dc
mrsharifull <116838065+mrsharifull@users.noreply.github.com>
2024-01-18 12:29:07 +0600

    Update

--- resources/views/admin/admin_management/permission/create.blade.php
+++ resources/views/admin/admin_management/permission/create.blade.php
@@ -39,6 +39,6 @@
                 </div>
             </div>
         </div>
-        @include('admin.partials.documentation'['document'=>$document])
+        @include('admin.partials.documentation',['document'=>$document])
     </div>
 @endsection
