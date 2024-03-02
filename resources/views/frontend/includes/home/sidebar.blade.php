c15fb2a95abe34ac8f193fc18c72ffbd2f7f41e0
Md Shariful Islam <mdsharifulislamm.info@gmail.com>
2024-03-02 20:53:23 +0600

    Category active --done

--- resources/views/frontend/includes/home/sidebar.blade.php
+++ resources/views/frontend/includes/home/sidebar.blade.php
@@ -4,7 +4,7 @@
         <h2 class="text-center cat-title">{{__('Categories')}}</h2>
         <div class="row mt-4 px-4">
             @foreach ($menuItems as $item)
-                <a href="{{route('category.products',$item->slug)}}" class="col-6 text-center single-cat text-decoration-none">
+                <a href="{{route('category.products',$item->slug)}}" class="col-6 text-center single-cat text-decoration-none {{(isset($category) && $category->id == $item->id) ? 'active' : ''}}">
                     <div class="icon m-auto">
                         <img class="w-100" src="{{storage_url($item->image) }}"
                             alt="category icon">
