3a7b0142171a2e5c876e708eb8bbdcd1255e89ee
Md Shariful Islam <mdsharifulislamm.info@gmail.com>
2024-03-24 17:02:11 +0600

    Updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,77 +1,387 @@
 @extends('frontend.layouts.master')
-@section('title','User Login')
+@section('title', 'User Login')
 
+@push('css')
+    <style>
+        img {
+            width: 100%;
+        }
+
+        .login_box {
+            width: 1050px;
+            height: 600px;
+            background: #fff;
+            border-radius: 10px;
+            box-shadow: 1px 4px 22px -8px #0004;
+            display: flex;
+            overflow: hidden;
+        }
+
+        .login_box .left {
+            width: 41%;
+            height: 100%;
+            padding: 25px 25px;
+            position: relative;
+
+        }
+
+        .login_box .right {
+            width: 59%;
+            height: 100%
+        }
+
+        .left .top_link a {
+            color: #452A5A;
+            font-weight: 400;
+        }
+
+        .left .top_link {
+            height: 20px
+        }
+
+        .left .contact {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            align-self: center;
+            height: 100%;
+            width: 73%;
+            margin: auto;
+        }
+
+        .left h3 {
+            text-align: center;
+            margin-bottom: 40px;
+        }
+
+        .left input {
+            border: none;
+            width: 80%;
+            margin: 15px 0px;
+            border-bottom: 1px solid #4f30677d;
+            padding: 7px 9px;
+            width: 100%;
+            overflow: hidden;
+            background: transparent;
+            font-weight: 600;
+            font-size: 14px;
+        }
+
+        .left {
+            background: linear-gradient(-45deg, #dcd7e0, #fff);
+        }
+
+        .submit {
+            border: none;
+            padding: 15px 70px;
+            border-radius: 8px;
+            display: block;
+            margin: auto;
+            margin-top: 120px;
+            background: var(--secondary-bg);
+            color: #fff;
+            font-weight: bold;
+            -webkit-box-shadow: 0px 9px 15px -11px rgba(88, 54, 114, 1);
+            -moz-box-shadow: 0px 9px 15px -11px rgba(88, 54, 114, 1);
+            box-shadow: 0px 9px 15px -11px rgba(88, 54, 114, 1);
+        }
+
+
+
+        .right {
+            background: linear-gradient(212.38deg, rgba(133, 255, 190, 0.5) 0%, rgba(45, 152, 218, 0.5) 100%), url('../frontend/asset/img/user_login_img.jpeg');
+            background-repeat: no-repeat;
+            background-size: cover;
+            color: #fff;
+            position: relative;
+        }
+
+        .right .right-text {
+            height: 100%;
+            position: relative;
+            transform: translate(0%, 45%);
+        }
+
+        .right-text h2 {
+            display: block;
+            width: 100%;
+            text-align: center;
+            font-size: 50px;
+            font-weight: 500;
+        }
+
+        .right-text h5 {
+            display: block;
+            width: 100%;
+            text-align: center;
+            font-size: 19px;
+            font-weight: 400;
+        }
+
+        .right .right-inductor {
+            position: absolute;
+            width: 70px;
+            height: 7px;
+            background: #fff0;
+            left: 50%;
+            bottom: 70px;
+            transform: translate(-50%, 0%);
+        }
+
+        .top_link img {
+            width: 28px;
+            padding-right: 7px;
+            margin-top: -3px;
+        }
+    </style>
+
+
+    <style>
+        .tabbed {
+            width: 93%;
+            min-width: 400px;
+            overflow: hidden;
+            transition: border 250ms ease;
+            position: absolute;
+            left: 13px;
+            top: 35px;
+            /* border-bottom: 4px solid var(--border-1); */
+        }
+
+        .tabbed ul {
+            margin: 0px;
+            padding: 0px;
+            overflow: hidden;
+            float: left;
+            padding-left: 48px;
+            list-style-type: none;
+        }
+
+        .tabbed ul * {
+            margin: 0px;
+            padding: 0px;
+        }
+
+        .tabbed ul li {
+            display: block;
+            float: right;
+            padding: 10px 5px 8px;
+            background-color: #FFF;
+            margin-right: 40px;
+            z-index: 2;
+            position: relative;
+            cursor: pointer;
+            color: #777;
+            text-transform: uppercase;
+            font: 600 13px/20px roboto, "Open Sans", Helvetica, sans-serif;
+            transition: all 250ms ease;
+        }
+
+        .tabbed ul li:before,
+        .tabbed ul li:after {
+            display: block;
+            content: " ";
+            position: absolute;
+            top: 0;
+            height: 100%;
+            width: 35px;
+            background-color: #FFF;
+            transition: all 250ms ease;
+            z-index: -1;
+        }
+
+        .tabbed ul li:before {
+            right: -24px;
+            transform: skew(30deg, 0deg);
+            box-shadow: rgba(0, 0, 0, .1) 3px 2px 5px, inset rgba(255, 255, 255, .09) -1px 0;
+        }
+
+        .tabbed ul li:after {
+            left: -24px;
+            transform: skew(-30deg, 0deg);
+            box-shadow: rgba(0, 0, 0, .1) -3px 2px 5px, inset rgba(255, 255, 255, .09) 1px 0;
+        }
+
+        .tabbed ul li:hover,
+        .tabbed ul li:hover:before,
+        .tabbed ul li:hover:after {
+            background-color: #F4F7F9;
+            color: #444;
+        }
+
+        .tabbed ul li.active {
+            z-index: 3;
+        }
+
+        .tabbed ul li.active,
+        .tabbed ul li.active:before,
+        .tabbed ul li.active:after {
+            background-color: var(--secondary-bg);
+            color: #fff;
+        }
+
+        /* Round Tabs */
+        .tabbed.round ul li {
+            border-radius: 8px 8px 0 0;
+        }
+
+        .tabbed.round ul li:before {
+            border-radius: 0 8px 0 0;
+        }
+
+        .tabbed.round ul li:after {
+            border-radius: 8px 0 0 0;
+        }
+    </style>
+@endpush
+
+{{-- @section('content')
+    <div class="container py-5 my-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Login') }}</div>
+                    <div class="card-header"><a href="{{ route('login_with_google') }}">{{ __('Login With Google') }}</a>
+                    </div>
+                    <div class="card-header"><a href="{{ route('login_with_github') }}">{{ __('Login With Github') }}</a>
+                    </div>
+                    <div class="card-header"><a href="{{ route('login_with_facebook') }}">{{ __('Login With Facebook') }}</a>
+                    </div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+
+                            <div class="row mb-3">
+                                <label for="phone"
+                                    class="col-md-4 col-form-label text-md-end">{{ __('Phone Number') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="phone" type="text"
+                                        class="form-control @error('phone') is-invalid @enderror" name="phone"
+                                        value="{{ old('phone') }}" required autocomplete="phone" autofocus>
+
+                                    @error('phone')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="password"
+                                    class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="password" type="password"
+                                        class="form-control @error('password') is-invalid @enderror" name="password"
+                                        required autocomplete="current-password">
+
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <div class="col-md-6 offset-md-4">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember"
+                                            {{ old('remember') ? 'checked' : '' }}>
+
+                                        <label class="form-check-label" for="remember">
+                                            {{ __('Remember Me') }}
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Login') }}
+                                    </button>
+
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                                    @endif
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection --}}
 @section('content')
-<div class="container py-5 my-5">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-                <div class="card-header"><a href="{{route('login_with_google')}}">{{ __('Login With Google') }}</a></div>
-                <div class="card-header"><a href="{{route('login_with_github')}}">{{ __('Login With Github') }}</a></div>
-                <div class="card-header"><a href="{{route('login_with_facebook')}}">{{ __('Login With Facebook') }}</a></div>
+    <div class="container py-5 my-5">
+        <div class="row justify-content-center">
+            <div class="col-md-10 mx-auto">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                <div class="login_box mx-auto">
 
-                        <div class="row mb-3">
-                            <label for="phone" class="col-md-4 col-form-label text-md-end">{{ __('Phone Number') }}</label>
+                    <div class="left">
+                        {{-- <div class="top_link"><a href="{{ route('home') }}">{{ __('Return home') }}</a></div> --}}
 
-                            <div class="col-md-6">
-                                <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone" autofocus>
 
+                        <div class="tabbed round">
+                            <ul>
+                                <li>{{ __('Login With OTP') }}</li>
+                                <li class="active">{{ __('Login With Password') }}</li>
+                            </ul>
+                        </div>
+                        <div class="contact">
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                {{-- <h3>{{ __('SIGN IN') }}</h3> --}}
+                                <input type="phone" name="phone" placeholder="PHONE">
                                 @error('phone')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
+                                <input type="text" name="password" placeholder="PASSWORD">
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
+                                <button class="submit">{{ __('LET\'S GO') }}</button>
+                            </form>
                         </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
+                    </div>
+                    <div class="right">
+                        <div class="right-text">
+                            <h2>{{ __('Dhaka Pharmacy') }}</h2>
+                            <h5>{{ __('User Login Portal') }}</h5>
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
+
+
             </div>
         </div>
     </div>
-</div>
 @endsection
+@push('js')
+   <script>
+    $(document).ready(function() {
+        $('.tabbed li').click(function() {
+            if ($(this).hasClass('active'))
+            return;
+
+            var parent = $(this).parent(),
+                innerTabs = parent.find('li');
+
+            innerTabs.removeClass('active');
+            $(this).addClass('active');
+        });
+    });
+</script> 
+@endpush
--- resources/views/frontend/product_order/checkout.blade.php
+++ resources/views/frontend/product_order/checkout.blade.php
@@ -3,100 +3,11 @@
 
 @push('css')
     <style>
-        .form-floating {
-            position: relative;
-        }
-
-        .form-floating>.form-control,
-        .form-floating>.form-select {
-            height: calc(3.625rem + 2px);
-            padding: 1.125rem 1.3125rem 0.9rem;
-        }
-
-        .form-label-fixed {
-            position: relative;
-        }
-
-        .form-label-fixed>.form-label {
-            position: absolute;
-            top: -1.00000625rem;
-            left: 1rem;
-            margin: 0;
-            padding: 0.25rem 0.5rem 0.25rem 0.5rem;
-            background-color: #ffffff;
-            color: #222222;
-            z-index: 1;
-        }
-
-        .form-control {
-            display: block;
-            width: 100%;
-            padding: 0.9375rem 0.9375rem 0.75rem;
-            font-size: 0.875rem;
-            font-weight: 400;
-            line-height: 1.7143;
-            color: #222222;
-            background-color: #fff;
-            background-clip: padding-box;
-            border: 0.125rem solid #e4e4e4;
-            appearance: none;
-            border-radius: 0;
-            box-shadow: none;
-            transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-        }
-
-        .form-control:focus {
-            color: #222222;
-            background-color: #fff;
-            border-color: #222222;
-            outline: 0;
-            box-shadow: none;
-        }
-
-        .dilling_details .form-floating>label,
-        .dilling_details .form-label-fixed>.form-label {
-            color: #767676;
-        }
-
-        .form-floating>.form-control:focus~label,
-        .form-floating>.form-control:not(:placeholder-shown)~label,
-        .form-floating>.form-select~label {
-            background-color: #ffffff;
-            color: #222222;
-            transform: translateY(-1.9rem);
-        }
-
-        .form-floating>.form-control:focus~label,
-        .form-floating>.form-control:not(:placeholder-shown)~label,
-        .form-floating>.form-select~label {
-            background-color: #ffffff;
-            color: #222222;
-            transform: translateY(-1.9rem);
-        }
-
-        .form-floating>label {
-            position: absolute;
-            top: 1rem;
-            height: auto;
-            left: 0.75rem;
-            padding: 0 0.5rem;
-            pointer-events: none;
-            border: 0.125rem solid transparent;
-            transform-origin: 0 0;
-            transition: opacity 0.1s ease-in-out, transform 0.1s ease-in-out;
-            color: #767676;
-        }
-
-        label {
-            display: inline-block;
-        }
-
         .checkout_products .image img {
             height: 75px;
             object-fit: cover;
         }
-
-        .checkout_products .details .product_title {
+         .checkout_products .details .product_title {
             font-size: 17px;
             color: var(--text-color-black);
         }
@@ -173,7 +84,7 @@
             border-radius: 100%;
             height: 25px;
             width: 25px;
-            top: 30px;
+            top: 42px;
             left: 20px;
             z-index: 5;
             transition: border .25s linear;
@@ -181,7 +92,9 @@
         }
 
         .payment_methods ul li:hover .check {
-            border: 5px solid #FFFFFF;
+            border: 5px solid #0DFF92;
+            background: #0DFF92;
+            color: #0DFF92;
         }
 
         .payment_methods ul li .check::before {
@@ -189,10 +102,11 @@
             position: absolute;
             content: '';
             border-radius: 100%;
-            height: 15px;
-            width: 15px;
-            top: 5px;
-            left: 5px;
+            height: 8px;
+            width: 8px;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
             margin: auto;
             transition: background 0.25s linear;
             -webkit-transition: background 0.25s linear;
@@ -209,40 +123,6 @@
         .payment_methods input[type=radio]:checked~label {
             color: #0DFF92;
         }
-
-        
-
-
-        /* Styles for alert...
-            by the way it is so weird when you look at your code a couple of years after you wrote it XD */
-
-        /* .alert {
-            box-sizing: border-box;
-            background-color: #BDFFE1;
-            width: 100%;
-            position: relative;
-            top: 0;
-            left: 0;
-            z-index: 300;
-            padding: 20px 40px;
-            color: #333;
-        }
-
-        .alert h2 {
-            font-size: 22px;
-            color: #232323;
-            margin-top: 0;
-        }
-
-        .alert p {
-            line-height: 1.6em;
-            font-size: 18px;
-        }
-
-        .alert a {
-            color: #232323;
-            font-weight: bold;
-        } */
     </style>
 @endpush
 
@@ -250,7 +130,7 @@
     <div class="row py-5 my-5">
         <div class="col-md-10 mx-auto">
             <div class="row">
-                <div class="col-md-5 order-md-2 mb-4">
+                <div class="col-md-7 order-md-1 mb-4">
                     <h4 class="d-flex justify-content-between align-items-center mb-3">
                         <span class="text-muted">Your Products</span>
                         <span class="badge badge-secondary badge-pill">3</span>
@@ -378,83 +258,9 @@
 
 
                 </div>
-                <div class="col-md-7 order-md-1 dilling_details">
-                    <h4 class="mb-3">{{ __('Billing Details') }}</h4>
+                <div class="col-md-5 order-md-2 billing_details">
                     <form class="needs-validation" novalidate>
                         <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-floating my-3">
-                                    <input type="text" class="form-control" id="checkout_first_name"
-                                        placeholder="First Name">
-                                    <label for="checkout_first_name">First Name</label>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-floating my-3">
-                                    <input type="text" class="form-control" id="checkout_last_name"
-                                        placeholder="Last Name">
-                                    <label for="checkout_last_name">Last Name</label>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="search-field my-3">
-                                    <div class="form-label-fixed hover-container">
-                                        <label for="country" class="form-label">Country / Region*</label>
-                                        <div class="js-hover__open">
-                                            <select name="country" class="form-control form-control-lg" id="country">
-                                                <option selected hidden>Select Country</option>
-                                                <option value="bangladesh">Bangladesh</option>
-                                                <option value="india">India</option>
-                                                <option value="pakistan">Pakistan</option>
-                                                <option value="united state">United State</option>
-                                                <option value="australia">Australia</option>
-                                                <option value="canada">Canada</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-floating mt-3 mb-3">
-                                    <textarea name="address" id="checkout_street_address" class="form-control" placeholder="Street Address *"></textarea>
-                                    <label for="checkout_company_name">Street Address *</label>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-floating my-3">
-                                    <input type="text" class="form-control" id="checkout_city"
-                                        placeholder="Town / City *">
-                                    <label for="checkout_city">Town / City *</label>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-floating my-3">
-                                    <input type="text" class="form-control" id="checkout_zipcode"
-                                        placeholder="Postcode / ZIP *">
-                                    <label for="checkout_zipcode">Postcode / ZIP *</label>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-floating my-3">
-                                    <input type="text" class="form-control" id="checkout_province"
-                                        placeholder="Province *">
-                                    <label for="checkout_province">Province *</label>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-floating my-3">
-                                    <input type="text" class="form-control" id="checkout_phone"
-                                        placeholder="Phone *">
-                                    <label for="checkout_phone">Phone *</label>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-floating my-3">
-                                    <input type="email" class="form-control" id="checkout_email"
-                                        placeholder="Your Mail *">
-                                    <label for="checkout_email">Your Mail *</label>
-                                </div>
-                            </div>
                             <div class="col-md-12">
                                 <div class="payment_methods">
 
@@ -478,8 +284,8 @@
                                             </div>
                                         </li>
                                         <li>
-                                            <input type="radio" id="s-option" name="selector">
-                                            <label for="s-option">Bank Transfer</label>
+                                            <input type="radio" id="b-option" name="selector">
+                                            <label for="b-option">Bank Transfer</label>
 
                                             <div class="check">
                                                 <div class="inside"></div>
