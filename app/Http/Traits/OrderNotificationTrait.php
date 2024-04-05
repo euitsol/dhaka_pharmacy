e727a6b11bf1dcbd04871b0607cc5618f6ef42c8
al-kafi-sohag-6 <85807115+al-kafi-sohag-6@users.noreply.github.com>
2024-04-05 21:22:42 +0600

    Update OrderNotificationTrait.php

--- app/Http/Traits/OrderNotificationTrait.php
+++ app/Http/Traits/OrderNotificationTrait.php
@@ -7,6 +7,7 @@ use App\Models\NotificationTemplate;
 use App\Models\User;
 use App\Notifications\Frontend\OrderNotification;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Log;
 
 trait OrderNotificationTrait{
 
@@ -14,6 +15,7 @@ trait OrderNotificationTrait{
     {
         $template = NotificationTemplate::where('key', $key)->first();
         if(!empty($template) && !empty($template->message)){
+            Log::alert($template);
             $placeholders = [
                 '{order-id}' => $order->order_id,
             ];
