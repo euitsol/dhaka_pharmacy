7af2a4efad931c40cb0377256d3315b9c8e7bfa1
Md Shariful Islam <mdsharifulislamm.info@gmail.com>
2024-05-22 20:45:30 +0600

    Updated

--- resources/views/rider/orders/details.blade.php
+++ resources/views/rider/orders/details.blade.php
@@ -108,23 +108,97 @@
                 <div class="card-footer">
                     @if($dor->status == 3)
                         <div class="buttons text-end">
-                            <a href="" class="btn btn-danger">{{__('Dispute')}}</a>
-                            <a href="" class="btn btn-primary">{{__('Collect')}}</a>
+                            <a href="javascript:void(0)" class="btn btn-danger dispute" data-class="dispute_form">{{__('Dispute')}}</a>
+                            <a href="javascript:void(0)" class="btn btn-primary collect" data-class="pharmacy_collect_otp_form">{{__('Collect')}}</a>
                         </div>
+                        <form style="display:none;" class="mt-3 form dispute_form" action="" method="POST">
+                            <div class="form-group dispute_note">
+                                <textarea name="dispute_note" class="form-control" placeholder="Enter dispute reason"></textarea>
+                            </div>
+                            @include('alerts.feedback', ['field' => 'dispute_note'])
+                            <div class="form-group text-end">
+                                <input type="submit" class="btn btn-secondary" value="Update">
+                            </div>
+                            
+                        </form>
+                        <form style="display:none;" class="mt-3 form pharmacy_collect_otp_form" action="" method="POST">
+                            <div class="form-group pharmacy_collect_otp">
+                                <input type="text" name="pharmacy_collect_otp" class="form-control" placeholder="Enter pharmacy otp">
+                            </div>
+                            @include('alerts.feedback', ['field' => 'pharmacy_collect_otp'])
+                            <div class="form-group text-end">
+                                <input type="submit" class="btn btn-secondary" value="Update">
+                            </div>
+                        </form>
                     @endif
                     @if($dor->status == 4)
-                        <a href="" class="btn btn-danger">{{__('Cancel')}}</a>
-                        <a href="" class="btn btn-primary">{{__('Delivered')}}</a>
+                        <a href="javascript:void(0)" class="btn btn-danger cancel" data-class="cancel_form">{{__('Cancel')}}</a>
+                        <a href="javascript:void(0)" class="btn btn-primary delivered" data-class="user_otp_form" >{{__('Delivered')}}</a>
+
+                        <form style="display:none;" class="mt-3 form cancel_form" action="" method="POST">
+                            <div class="form-group cancel_note">
+                                <textarea name="cancel_note" class="form-control" placeholder="Enter cancel reason"></textarea>
+                            </div>
+                            @include('alerts.feedback', ['field' => 'cancel_note'])
+                            <div class="form-group text-end">
+                                <input type="submit" class="btn btn-secondary" value="Update">
+                            </div>
+                        </form>
+                        <form style="display:none;" class="mt-3 form user_otp_form" action="" method="POST">
+                            <div class="form-group user_otp">
+                                <input type="text" name="user_otp" class="form-control" placeholder="Enter user otp">
+                            </div>
+                            @include('alerts.feedback', ['field' => 'user_otp'])
+                            <div class="form-group text-end">
+                                <input type="submit" class="btn btn-secondary" value="Update">
+                            </div>
+                        </form>
                     @endif
                     @if($dor->status == 5)
-                        <a href="" class="btn btn-primary">{{__('Complete')}}</a>
+                        <a href="javascript:void(0)" class="btn btn-primary complete" data-class="transaction_form">{{__('Complete')}}</a>
+                        <form style="display:none;" class="mt-3 form transaction_form" action="" method="POST">
+                            <div class="form-group transaction">
+                                <input type="text" name="transaction" class="form-control" placeholder="Enter transaction id">
+                            </div>
+                            @include('alerts.feedback', ['field' => 'transaction'])
+                            <div class="form-group text-end">
+                                <input type="submit" class="btn btn-secondary" value="Update">
+                            </div>
+                        </form>
                     @endif
                     @if($dor->status == 7)
-                        <a href="" class="btn btn-primary">{{__('Cancel Complete')}}</a>
+                        <a href="javascript:void(0)" class="btn btn-primary cancel_complete" data-class="pharmacy_return_otp_form">{{__('Cancel Complete')}}</a>
+                        <form style="display:none;" class="mt-3 form pharmacy_return_otp_form" action="" method="POST">
+                            <div class="form-group pharmacy_return_otp">
+                                <input type="text" name="pharmacy_return_otp" class="form-control" placeholder="Enter pharmacy return otp">
+                            </div>
+                            @include('alerts.feedback', ['field' => 'pharmacy_return_otp'])
+                            <div class="form-group text-end">
+                                <input type="submit" class="btn btn-secondary" value="Update">
+                            </div>
+                        </form>
                     @endif
                 </div>
             </div>
         </div>
     </div>
 @endsection
+@push('js')
+<script>
+    $(document).ready(function() {
+        function showForm(element) {
+            $('.form').each(function() {
+                $(this).hide();
+            });
+            var form_class = element.data('class');
+            $('.' + form_class).show();
+        }
+
+        $(document).on('click', '.dispute, .collect, .cancel, .delivered, .complete, .cancel_complete', function() {
+            showForm($(this));
+        });
+    });
+</script>
+    
+@endpush
 
