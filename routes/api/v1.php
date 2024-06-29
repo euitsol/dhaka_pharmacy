9ecdb5bedcb8fb651ad054ce314c938508b93ca2
Md Shariful Islam <mdsharifulislamm.info@gmail.com>
2024-06-29 15:46:02 +0600

    Cart API --Done

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\User\AddressController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\User\AuthenticationController;
+use App\Http\Controllers\Api\User\CartAjaxController;
 use App\Http\Controllers\Api\User\UserController;
 
 Route::group(['as' => 'u.', 'prefix' => 'user'], function () {
@@ -13,7 +14,6 @@ Route::group(['as' => 'u.', 'prefix' => 'user'], function () {
         Route::post('password-login', 'pass_login')->name('l.p');
         Route::post('send-otp', 'send_otp')->name('s.o');
         Route::post('verify-otp', 'otp_verify')->name('v.o');
-
         Route::post('registration', 'registration')->name('reg');
     });
 
@@ -26,7 +26,17 @@ Route::group(['as' => 'u.', 'prefix' => 'user'], function () {
         Route::post('/store', 'store')->name('store');
         Route::post('/update', 'update')->name('update');
     });
+    // Cart API 
+    Route::controller(CartAjaxController::class)->middleware('auth:api-user')->prefix('cart')->name('cart.')->group(function () {
+        Route::post('add', 'add')->name('add');
+        Route::get('products', 'products')->name('products');
+        Route::post('update', 'update')->name('update');
+        Route::post('delete', 'delete')->name('delete');
+    });
 });
+
+
+
 
 Route::group(['as' => 'f.', 'prefix' => 'frontend'], function () {
     Route::controller(CategoryController::class)->prefix('categories')->name('cats.')->group(function () {
