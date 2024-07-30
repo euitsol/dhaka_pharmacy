60656677e95ec0d5c499e2824567bc94345d7556
Md Shariful Islam <mdsharifulislamm.info@gmail.com>
2024-07-30 14:50:08 +0600

    Login Page & Footer Update

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -11,8 +11,8 @@
                                 <img src="{{ asset('frontend/asset/img/delivery.png') }}" alt="">
                             </div>
                             <div class="col-10">
-                                <h3>Quick Delivery</h3>
-                                <h4>Varius sit amet mattis vulputat</h4>
+                                <h3>{{ __('Quick Delivery') }}</h3>
+                                <h4>{{ __('Varius sit amet mattis vulputat') }}</h4>
                             </div>
                         </div>
                     </div>
@@ -22,8 +22,8 @@
                                 <img src="{{ asset('frontend/asset/img/clock.png') }}" alt="">
                             </div>
                             <div class="col-10">
-                                <h3>24/7 Help Center</h3>
-                                <h4>Dedicated 24/7 support</h4>
+                                <h3>{{ __('24/7 Help Center') }}</h3>
+                                <h4>{{ __('Dedicated 24/7 support') }}</h4>
                             </div>
                         </div>
                     </div>
@@ -33,8 +33,8 @@
                                 <img src="{{ asset('frontend/asset/img/setisfide.png') }}" alt="">
                             </div>
                             <div class="col-10">
-                                <h3>Satisfied or Refunder</h3>
-                                <h4>VFree returns within 14 days</h4>
+                                <h3>{{ __('Satisfied or Refunder') }}</h3>
+                                <h4>{{ __('Free returns within 14 days') }}</h4>
                             </div>
                         </div>
                     </div>
@@ -44,8 +44,8 @@
                                 <img src="{{ asset('frontend/asset/img/card.png') }}" alt="">
                             </div>
                             <div class="col-9">
-                                <h3>Secured Payment</h3>
-                                <h4>Pharetra magna ac placerat vestibu</h4>
+                                <h3>{{ __('Secured Payment') }}</h3>
+                                <h4>{{ __('Pharetra magna ac placerat vestibu') }}</h4>
                             </div>
                         </div>
                     </div>
@@ -60,7 +60,8 @@
                     <div class="col-8 m-auto">
                         <div class="row align-items-center">
                             <div class="col-4">
-                                <h3 class="title text-white m-0 text-center">Subscribe to get the Latest News</h3>
+                                <h3 class="title text-white m-0 text-center">
+                                    {{ __('Subscribe to get the Latest News') }}</h3>
                             </div>
                             <div class="col-8">
                                 <form action="" class="d-flex align-items-center">
@@ -83,7 +84,7 @@
                                     alt="Footer logo"></a>
                         </div>
                         <div class="social-info mb-4">
-                            <h3 class="title">Follow Us</h3>
+                            <h3 class="title">{{ __('Follow Us') }}</h3>
                             <div class="icons d-flex">
                                 <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
                                 <a href="#"><i class="fa-brands fa-twitter"></i></a>
@@ -92,7 +93,7 @@
                             </div>
                         </div>
                         <div class="pay-info">
-                            <h3 class="title">We Accept</h3>
+                            <h3 class="title">{{ __('We Accept') }}</h3>
                             <div class="payment">
                                 <img src="{{ asset('frontend/asset/img/card-update-logo.png') }}"
                                     alt="Payment card image">
@@ -100,45 +101,44 @@
                         </div>
                     </div>
                     <div class="col-3">
-                        <h2 class="title">Information</h2>
+                        <h2 class="title">{{ __('Information') }}</h2>
                         <div class="footer-menu">
                             <ul class="footer-nav">
-                                <li><a href="#">Home</a></li>
-                                <li><a href="#">About Us</a></li>
-                                <li><a href="#">Order</a></li>
-                                <li><a href="#">Register</a></li>
-                                <li><a href="#">Login</a></li>
+                                <li><a href="{{ route('home') }}">{{ __('Home') }}</a></li>
+                                <li><a href="#">{{ __('About Us') }}</a></li>
+                                <li><a href="#">{{ __('Contact Us') }}</a></li>
+                                @if (!Auth::guard('web')->check())
+                                    <li><a href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                                @endif
                             </ul>
                         </div>
                     </div>
                     <div class="col-3">
-                        <h2 class="title">Customer Service</h2>
+                        <h2 class="title">{{ __('Customer Service') }}</h2>
                         <div class="footer-menu">
                             <ul class="footer-nav">
-                                <li><a href="#">FAQ</a></li>
-                                <li><a href="#">Terms and Conditions
-                                    </a></li>
-                                <li><a href="#">Order History
-                                    </a></li>
-                                <li><a href="#">Privacy Policy
-                                    </a></li>
-                                <li><a href="#">Contact Us</a></li>
+                                <li><a href="#">{{ __('FAQ') }}</a></li>
+                                <li><a href="#">{{ __('Terms and Conditions') }}</a></li>
+                                <li><a href="#">{{ __('Privacy Policy') }}</a></li>
+                                @if (!Auth::guard('web')->check())
+                                    <li><a href="{{ route('use.register') }}">{{ __('Register') }}</a></li>
+                                @endif
                             </ul>
                         </div>
                     </div>
                     <div class="col-3">
-                        <h2 class="title">Your trusted online medicine supplier</h2>
+                        <h2 class="title">{{ __('Your trusted online medicine supplier') }}</h2>
                         <div class="footer-contact">
                             <ul class="footer-nav">
-                                <li><a class="map" href="https://maps.app.goo.gl/UPWoFht1PicYMHdR8"
-                                        target="_blank">Road -4, 11 Gulshan Avenue, Dhaka BangladeshFree
-                                        Consultation</a></li>
-                                <li><a class="phn"
-                                        href="tel:01714 432 534
-                                        ">01714 432 534
-                                    </a></li>
-                                <li><a class="mail"
-                                        href="mailto:admin@dhakapharmacy.com.bd">admin@dhakapharmacy.com.bd</a></li>
+                                <li><a class="map" href="https://maps.app.goo.gl/UPWoFht1PicYMHdR8" target="_blank">
+                                        {{ __('Road -4, 11 Gulshan Avenue, Dhaka BangladeshFree Consultation') }}
+                                    </a>
+                                </li>
+                                <li><a class="phn" href="tel:01714 432 534">{{ __('01714 432 534') }}</a></li>
+                                <li>
+                                    <a class="mail"
+                                        href="mailto:admin@dhakapharmacy.com.bd">{{ __('admin@dhakapharmacy.com.bd') }}</a>
+                                </li>
                             </ul>
                         </div>
                     </div>
@@ -151,7 +151,8 @@
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <p class="text-center m-0">{{date('Y')}} &copy all rights reserved by Dhaka Pharmacy</p>
+                    <p class="text-center m-0">
+                        {{ date('Y') }} &copy{{ __(' all rights reserved by Dhaka Pharmacy') }}</p>
                 </div>
             </div>
         </div>
