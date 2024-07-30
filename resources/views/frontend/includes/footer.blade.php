d95b3afef347b984116238a32fa1469fe9cd67eb
Md Shariful Islam <mdsharifulislamm.info@gmail.com>
2024-07-30 15:48:51 +0600

    Privacy Policy Frontend --Done

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -119,7 +119,7 @@
                             <ul class="footer-nav">
                                 <li><a href="{{ route('faq') }}">{{ __('FAQ') }}</a></li>
                                 <li><a href="#">{{ __('Terms and Conditions') }}</a></li>
-                                <li><a href="#">{{ __('Privacy Policy') }}</a></li>
+                                <li><a href="{{ route('privacy_policy') }}">{{ __('Privacy Policy') }}</a></li>
                                 @if (!Auth::guard('web')->check())
                                     <li><a href="{{ route('use.register') }}">{{ __('Register') }}</a></li>
                                 @endif
