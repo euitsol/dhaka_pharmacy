c25a8c2ab9e3130b65a1eb5f67ff9bfb37d27aa2
Md Shariful Islam <mdsharifulislamm.info@gmail.com>
2024-08-19 15:15:42 +0600

    Order List API --Done

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -44,6 +44,8 @@ Route::group(['as' => 'u.', 'prefix' => 'user'], function () {
         Route::post('initiat/single', 'int_single_order')->name('s.i');
         Route::get('details', 'details')->name('d');
         Route::post('confirm', 'order_confirm')->name('c');
+
+        Route::get('list', 'list')->name('l');
     });
 });
 
