ac065895e18b86a6b284417b3a190dfe33e879d7
Md Shariful Islam <mdsharifulislamm.info@gmail.com>
2024-09-08 18:09:57 +0600

    Update status Title

--- resources/views/user/order/list.blade.php
+++ resources/views/user/order/list.blade.php
@@ -6,7 +6,7 @@
             <div class="row">
                 <div class="col">
                     <div class="page-title">
-                        <h3>{{ __(isset($status) ? $status : 'My Orders') }}</h3>
+                        <h3>{{ __(isset($status) ? slugToTitle($status) : 'My Orders') }}</h3>
                     </div>
                     <div class="show-order d-flex align-items-center">
                         <h4 class="me-2">{{ __('Show:') }}</h4>
