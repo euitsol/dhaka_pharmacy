665e39e29b00ffd0e5dab94ef66f3b70e5454fad
Md Shariful Islam <116838065+devshariful99@users.noreply.github.com>
2025-01-06 15:51:06 +0600

    Contact Page & About Page --done

---
+++ app/Http/Controllers/Frontend/ContactPageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+
+class ContactPageController extends Controller
+{
+    public function contact(): View
+    {
+        return view('frontend.contact');
+    }
+}
