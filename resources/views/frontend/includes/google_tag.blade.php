f780eef7322938060fd6f1f9531848b1faedf37e
al-kafi-sohag <85807115+al-kafi-sohag@users.noreply.github.com>
2025-02-19 10:39:46 +0600

    google analytics tag added

---
+++ resources/views/frontend/includes/google_tag.blade.php
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FKB1PJSQPY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FKB1PJSQPY');
+</script>
