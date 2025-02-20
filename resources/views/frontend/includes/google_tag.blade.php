cd52af44189812d6d01385aa28cb79356cf8b5dd
Md Shariful Islam <116838065+devshariful99@users.noreply.github.com>
2025-02-20 18:29:10 +0600

    Delete resources/views/frontend/includes/google_tag.blade.php

--- resources/views/frontend/includes/google_tag.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-FKB1PJSQPY"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-FKB1PJSQPY');
-</script>
