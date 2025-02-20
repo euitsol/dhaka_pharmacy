e45113cbace5a45e638d881d5d5991539b0044d0
al-kafi-sohag <85807115+al-kafi-sohag@users.noreply.github.com>
2025-02-20 16:33:08 +0600

    init steadfast

--- app/Services/OrderDeliveryService.php
+++ app/Services/OrderDeliveryService.php
@@ -2,7 +2,44 @@
 
 namespace App\Services;
 
+use App\Models\Order;
+use App\Services\SteadFastService;
+use App\Models\OrderHub;
+
 class OrderDeliveryService
 {
+    protected SteadFastService$steadFastService;
+    protected OrderHub $orderHub;
+
+    public function __construct(SteadFastService $steadFastService)
+    {
+        $this->steadFastService = $steadFastService;
+    }
+
+    public function processDelivery(string $type)
+    {
+        $this->steadFastService->createShipment($this->orderHub);
+    }
+
+    public function validateDelivery()
+    {
+        if(!$this->orderHub){
+            throw new \InvalidArgumentException("Order hub not found");
+        }
+
+        if(!$this->orderHub->order){
+            throw new \InvalidArgumentException("Order not found");
+        }
+
+        if(!$this->orderHub->hub){
+            throw new \InvalidArgumentException("Hub not found");
+        }
+
+        if(!$this->orderHub->order->status != Order::PACHAGE_PREPARED){
+            throw new \InvalidArgumentException("Order must be in Prepared status to process delivery");
+        }
+
+    }
+
 
 }
