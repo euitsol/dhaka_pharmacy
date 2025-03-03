8173853ef0f49dcf77b309e537e1097d6e061862
Roy <160618208+roynironjon@users.noreply.github.com>
2025-03-03 15:38:44 +0600

    home page medical devices product border issue update done

--- resources/views/user/dashboard/dashboard.blade.php
+++ resources/views/user/dashboard/dashboard.blade.php
@@ -216,7 +216,7 @@
                         <div class="col-xl-9 col-12">
                             <div class="col-right row row-gap-3 row-gap-sm-4 ">
                                 <div class="col-lg-6 col-12 order-2 order-lg-1">
-                                    <div class="google-map" @if ($latest_offers->isEmpty()) style="margin-top:20px"
+                                    <div class="google-map" @if ($latest_offers->isEmpty())
                                         @endif>
                                         <div class="address d-flex  align-items-center justify-content-between">
                                             <div class="title">
