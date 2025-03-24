fe787f7af57255c4df0dd115f5a39af27f5b7c72
Md Shariful Islam <116838065+devshariful99@users.noreply.github.com>
2025-03-24 12:48:55 +0600

    LAM and DM Dashboard menu updated

--- resources/views/local_area_manager/partials/navbars/sidebar.blade.php
+++ resources/views/local_area_manager/partials/navbars/sidebar.blade.php
@@ -12,29 +12,10 @@
                     <p>{{ __('Dashboard') }}</p>
                 </a>
             </li>
-            <li @if ($pageSlug == 'local_area_manager_profile') class="active" @endif>
-                <a href="{{ route('lam.profile.index') }}">
-                    <i class="fa-solid fa-user @if ($pageSlug == 'local_area_manager_profile') fa-beat-fade @endif"></i>
-                    <p>{{ __('My Profile') }}</p>
-                </a>
-            </li>
-
-            <li @if ($pageSlug == 'kyc_verification') class="active" @endif>
-                <a href="{{ route('lam.kyc.verification') }}">
-                    <i class="fa-solid fa-shield-alt @if ($pageSlug == 'kyc_verification') fa-beat-fade @endif"></i>
-                    <p>{{ __('KYC Verification Center') }}</p>
-                </a>
-            </li>
             <li @if ($pageSlug == 'user') class="active" @endif>
                 <a href="{{ route('lam.user.list') }}">
                     <i class="fa-solid fa-users @if ($pageSlug == 'user') fa-beat-fade @endif"></i>
                     <p>{{ __('User Management') }}</p>
-                </a>
-            </li>
-            <li @if ($pageSlug == 'operational_area') class="active" @endif>
-                <a href="{{ route('lam.operational_area.list') }}">
-                    <i class="fa-solid fa-earth-americas @if ($pageSlug == 'operational_area') fa-beat-fade @endif"></i>
-                    <p>{{ __('Operation Areas') }}</p>
                 </a>
             </li>
             <li @if ($pageSlug == 'wm') class="active" @endif>
@@ -48,6 +29,24 @@
                     <i
                         class="fa-solid fa-hand-holding-dollar @if ($pageSlug == 'earning') fa-beat-fade @endif"></i>
                     <p>{{ __('My Earnings') }}</p>
+                </a>
+            </li>
+            <li @if ($pageSlug == 'kyc_verification') class="active" @endif>
+                <a href="{{ route('lam.kyc.verification') }}">
+                    <i class="fa-solid fa-shield-alt @if ($pageSlug == 'kyc_verification') fa-beat-fade @endif"></i>
+                    <p>{{ __('KYC Verification Center') }}</p>
+                </a>
+            </li>
+            <li @if ($pageSlug == 'local_area_manager_profile') class="active" @endif>
+                <a href="{{ route('lam.profile.index') }}">
+                    <i class="fa-solid fa-user @if ($pageSlug == 'local_area_manager_profile') fa-beat-fade @endif"></i>
+                    <p>{{ __('My Profile') }}</p>
+                </a>
+            </li>
+            <li @if ($pageSlug == 'operational_area') class="active" @endif>
+                <a href="{{ route('lam.operational_area.list') }}">
+                    <i class="fa-solid fa-earth-americas @if ($pageSlug == 'operational_area') fa-beat-fade @endif"></i>
+                    <p>{{ __('Operation Areas') }}</p>
                 </a>
             </li>
             <li @if ($pageSlug == 'feedback') class="active" @endif>
